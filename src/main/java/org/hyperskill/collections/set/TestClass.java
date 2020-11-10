9098967e0ef1e9af2beae44ee572229ed144e25c
Васильев Дмитрий <lotus33497@yandex.ru>
2020-11-10 21:25:48 +0700

    add and connect maven checkstyle and travis Travis CI

--- src/main/java/org/hyperskill/collections/set/TestClass.java
+++ src/main/java/org/hyperskill/collections/set/TestClass.java
@@ -1,6 +1,6 @@
 package main.java.org.hyperskill.collections.set;
 
-public class testClass {
+public class TestClass {
     public static void main(String[] args) {
         System.out.println("none");
     }
---
+++ src/test/java/org/hyperskill/SpellCheckerTest.java
@@ -0,0 +1,4 @@
+package test.java.org.hyperskill;
+
+public class SpellCheckerTest {
+}
